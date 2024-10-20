03ef370c3e52fee3eb26a9475ef2d678952d63be
terezahr <91895985+terezahr@users.noreply.github.com>
2024-10-20 09:04:55 +0200

    add interval dialog

---
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/IntervalDialog.java
@@ -0,0 +1,40 @@
+package cz.muni.fi.pv168.project.ui.dialog;
+
+import cz.muni.fi.pv168.project.model.TimeUnit;
+
+import javax.swing.*;
+
+public final class IntervalDialog extends EntityDialog<TimeUnit> {
+
+    private final JTextField nameField = new JTextField();
+    private final JTextField shortcutField = new JTextField();
+    private final JTextField minutesField = new JTextField();
+
+    private final TimeUnit timeUnit;
+
+    public IntervalDialog(TimeUnit timeUnit) {
+        this.timeUnit = timeUnit;
+        setValues();
+        addFields();
+    }
+
+    private void setValues() {
+        nameField.setText(timeUnit.getName());
+        shortcutField.setText(timeUnit.getShortcut());
+        minutesField.setText(String.valueOf(timeUnit.getMinutes()));
+    }
+
+    private void addFields() {
+        add("Name:", nameField);
+        add("Shortcut:", shortcutField);
+        add("Minutes:", minutesField);
+    }
+
+    @Override
+    TimeUnit getEntity() {
+        timeUnit.setName(nameField.getText());
+        timeUnit.setShortcut(shortcutField.getText());
+        timeUnit.setMinutes(Integer.parseInt(minutesField.getText()));
+        return timeUnit;
+    }
+}
