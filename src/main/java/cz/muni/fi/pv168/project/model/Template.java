e019c11a7490101f75508a6079bfc15412bd8b19
terezahr <91895985+terezahr@users.noreply.github.com>
2024-10-23 09:08:05 +0200

    display template name

--- src/main/java/cz/muni/fi/pv168/project/model/Template.java
+++ src/main/java/cz/muni/fi/pv168/project/model/Template.java
@@ -95,4 +95,9 @@ public class Template {
     public TodoEvent toTodoEvent(){
             return new TodoEvent(name,details, date,timeUnit, timeUnitAmount, categories );
     }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
