bc7d4b17a35a0c07bce8d04642fe8d819dfb88af
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2024-11-03 22:18:53 +0100

    Remove unnecessary complexity from color chooser

--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/CategoryDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/CategoryDialog.java
@@ -3,22 +3,34 @@ package cz.muni.fi.pv168.project.ui.dialog;
 import cz.muni.fi.pv168.project.model.Category;
 
 import javax.swing.*;
+import javax.swing.colorchooser.AbstractColorChooserPanel;
 import java.awt.*;
-
+import java.util.Locale;
 public final class CategoryDialog extends EntityDialog<Category> {
 
     private final JTextField nameField = new JTextField();
-    //private final JComboBox<Color> colorComboBox = new JComboBox<>(Color.values());
 
-    private final JColorChooser colorChooser = new JColorChooser();
+    private static final JColorChooser colorChooser = new JColorChooser();
 
     private final Category category;
     private Color selectedColor;
 
     public CategoryDialog(Category category) {
         this.category = category;
+
+        setupColorChooser();
         setValues();
         addFields();
+
+
+    }
+
+    private void setupColorChooser(){
+        AbstractColorChooserPanel[] panels = colorChooser.getChooserPanels();
+        for (int i = 1; i < panels.length; i++) {
+            colorChooser.removeChooserPanel(panels[i]);
+        }
+        colorChooser.setPreviewPanel(new JPanel());
     }
 
     private void setValues() {
