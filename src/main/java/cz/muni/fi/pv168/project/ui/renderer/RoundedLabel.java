6cac17702e442a61b0dfc47c1fe35198d3631436
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2024-11-04 18:45:28 +0100

    Add contrasting colors for darker shades

--- src/main/java/cz/muni/fi/pv168/project/ui/renderer/RoundedLabel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/renderer/RoundedLabel.java
@@ -7,17 +7,28 @@ import java.awt.*;
 
 public class RoundedLabel extends JLabel {
     private Color backgroundColor;
+    private Color foregroundColor;
     private final int cornerRadius = 10;
-
     private final int padding = 5;
-
 
     public RoundedLabel(String text, Color backgroundColor){
         super(text);
         super.setBorder(new EmptyBorder(0, padding, 0, padding));
 
         this.backgroundColor = backgroundColor;
+        this.foregroundColor = getContrastColor();
+
         setOpaque(false);
+    }
+
+    private Color getContrastColor(){
+        var r = backgroundColor.getRed();
+        var g = backgroundColor.getGreen();
+        var b = backgroundColor.getBlue();
+
+        var color =  ((r*299)+(g*587)+(b*114))/1000;
+
+        return (color >= 128) ? Color.BLACK : Color.WHITE;
     }
 
     @Override
@@ -32,11 +43,8 @@ public class RoundedLabel extends JLabel {
 
         g2d.fillRoundRect(0, 0 , getWidth(), getHeight(), cornerRadius, cornerRadius);
 
-
-
+        setForeground(foregroundColor);
         super.paintComponent(g);
         g2d.dispose();
     }
-
-
 }
