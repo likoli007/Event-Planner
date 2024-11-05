27d2787944853c91136201ffdaa13c26d475f8ee
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2024-11-05 09:33:35 +0100

    Refactor and cleanup of RoundedLabel

--- src/main/java/cz/muni/fi/pv168/project/ui/renderer/RoundedLabel.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/renderer/RoundedLabel.java
@@ -22,11 +22,11 @@ public class RoundedLabel extends JLabel {
     }
 
     private Color getContrastColor(){
-        var r = backgroundColor.getRed();
-        var g = backgroundColor.getGreen();
-        var b = backgroundColor.getBlue();
+        int r = backgroundColor.getRed();
+        int g = backgroundColor.getGreen();
+        int b = backgroundColor.getBlue();
 
-        var color =  ((r*299)+(g*587)+(b*114))/1000;
+        int color =  ((r*299)+(g*587)+(b*114))/1000;
 
         return (color >= 128) ? Color.BLACK : Color.WHITE;
     }
@@ -35,10 +35,7 @@ public class RoundedLabel extends JLabel {
     protected void paintComponent(Graphics g) {
         Graphics2D g2d = (Graphics2D) g.create();
 
-
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-
-
         g2d.setColor(backgroundColor);
 
         g2d.fillRoundRect(0, 0 , getWidth(), getHeight(), cornerRadius, cornerRadius);
