49ad710a8afd878d254bdeb464951b79721f5197
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2024-11-07 18:04:44 +0100

    Fix unused imports

--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/TodoEventDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/TodoEventDialog.java
@@ -4,7 +4,6 @@ import com.github.lgooddatepicker.components.DatePicker;
 import com.github.lgooddatepicker.components.TimePicker;
 import cz.muni.fi.pv168.project.data.TestDataGenerator;
 import cz.muni.fi.pv168.project.model.*;
-import cz.muni.fi.pv168.project.model.Color;
 import cz.muni.fi.pv168.project.ui.model.AllTableModels;
 import cz.muni.fi.pv168.project.ui.model.TemplateTableModel;
 
