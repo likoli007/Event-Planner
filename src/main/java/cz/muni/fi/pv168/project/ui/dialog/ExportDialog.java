3e44e8baf97adc950af14d9a4372146b5fd57684
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2024-11-09 12:37:28 +0100

    Redo export dialog look

--- src/main/java/cz/muni/fi/pv168/project/ui/dialog/ExportDialog.java
+++ src/main/java/cz/muni/fi/pv168/project/ui/dialog/ExportDialog.java
@@ -1,115 +1,142 @@
 package cz.muni.fi.pv168.project.ui.dialog;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.filechooser.FileSystemView;
-import javax.swing.filechooser.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.io.File;
 
-public class ExportDialog{
-
-    private JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-    private FileNameExtensionFilter filter = new FileNameExtensionFilter("JSON Files", "json");
-
-    JPanel dialogPanel = new JPanel(new BorderLayout());
-    JPanel exportPanel = new JPanel(new BorderLayout());
-    JPanel exportOptionsPanel = new JPanel(new BorderLayout());
-    JLabel filePathLabel = new JLabel("File Path:");
-    JButton openButton = new JButton("Open");
-    JTextField textField = new JTextField();
-    JButton exportButton = new JButton("Export");
-
-    JRadioButton filterRadioButton = new JRadioButton("Export filtered");
-    JRadioButton noFilterRadioButton = new JRadioButton("Export all");
-
-    ButtonGroup filterGroup = new ButtonGroup();
-    JLabel exportStatisticsLabel = new JLabel();
+public class ExportDialog {
+    private final JFileChooser fileChooser;
+    private final FileNameExtensionFilter filter;
+    private final JPanel mainPanel;
+    private final JPanel exportPanel;
+    private final JPanel exportOptionsPanel;
+    private final JLabel filePathLabel;
+    private final JButton openButton;
+    private final JTextField textField;
+    private final JButton exportButton;
+    private final JRadioButton filterRadioButton;
+    private final JRadioButton noFilterRadioButton;
+    private final ButtonGroup filterGroup;
+    private final JLabel exportStatisticsLabel;
+    private final JPanel radioPanel;
+    private final JPanel buttonPanel;
 
     private String resultFilePath;
     private boolean exportFiltered;
 
-    private JDialog dialog;
+    private final JDialog dialog;
 
     public ExportDialog(JFrame parentFrame) {
-        dialog = new JDialog(parentFrame, "Export", true);
-        dialog.setSize(300, 150);
-        dialog.setLocationRelativeTo(parentFrame);
+        this.fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+        this.filter = new FileNameExtensionFilter("JSON Files", "json");
 
-        textField.setEditable(false);
-        openButton.addActionListener(this::openButtonClicked);
+        this.mainPanel = new JPanel();
+        this.exportPanel = new JPanel(new FlowLayout());
+        this.exportOptionsPanel = new JPanel(new BorderLayout());
+        this.filePathLabel = new JLabel("File Path:");
+        this.openButton = new JButton("Open");
+        this.textField = new JTextField();
+        this.exportButton = new JButton("Export");
+        this.filterRadioButton = new JRadioButton("Export filtered");
+        this.noFilterRadioButton = new JRadioButton("Export all");
+        this.filterGroup = new ButtonGroup();
+        this.exportStatisticsLabel = new JLabel();
+        this.radioPanel = new JPanel();
+        this.buttonPanel = new JPanel();
 
+        this.dialog = new JDialog(parentFrame, "Export", true);
+        this.dialog.setSize(300, 150);
+        this.dialog.setLocationRelativeTo(parentFrame);
+        this.dialog.setResizable(false);
+
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
         exportButton.addActionListener(this::exportButtonClicked);
 
-        filterGroup.add(filterRadioButton);
+        textField.setEditable(true);
+        textField.setSize(200, 20);
+        textField.setPreferredSize(new Dimension(200, 20));
+        openButton.addActionListener(this::openButtonClicked);
+
+        exportPanel.add(filePathLabel);
+        exportPanel.add(textField);
+        exportPanel.add(openButton);
+
         filterGroup.add(noFilterRadioButton);
+        filterGroup.add(filterRadioButton);
 
         JLabel filterLabel = new JLabel("Events export options:");
+        filterLabel.setBorder(new EmptyBorder(10, 5, 5, 0));
+        exportStatisticsLabel.setBorder(new EmptyBorder(0, 5, 5, 0));
+        exportStatisticsLabel.setVisible(true);
+        exportStatisticsLabel.setText("Will export 90 events.");
 
-        exportOptionsPanel.add(filterLabel, BorderLayout.NORTH);
-        exportOptionsPanel.add(filterRadioButton, BorderLayout.WEST);
-        exportOptionsPanel.add(noFilterRadioButton, BorderLayout.CENTER);
-        exportOptionsPanel.add(exportStatisticsLabel, BorderLayout.SOUTH);
-
-        exportStatisticsLabel.setVisible(false);
         filterRadioButton.addActionListener(this::filterRadioButtonClicked);
         noFilterRadioButton.addActionListener(this::noFilterRadioButtonClicked);
+        noFilterRadioButton.setSelected(true);
+        radioPanel.add(noFilterRadioButton);
+        radioPanel.add(filterRadioButton);
 
-        exportPanel.add(filePathLabel, BorderLayout.WEST);
-        exportPanel.add(textField, BorderLayout.CENTER);
-        exportPanel.add(openButton, BorderLayout.EAST);
+        exportOptionsPanel.add(filterLabel, BorderLayout.NORTH);
+        exportOptionsPanel.add(radioPanel, BorderLayout.CENTER);
+        exportOptionsPanel.add(exportStatisticsLabel, BorderLayout.SOUTH);
 
-        dialogPanel.add(exportOptionsPanel, BorderLayout.CENTER);
-        dialogPanel.add(exportPanel, BorderLayout.NORTH);
-        dialogPanel.add(exportButton, BorderLayout.SOUTH);
+        buttonPanel.setLayout(new FlowLayout());
+        buttonPanel.add(exportButton);
 
-        dialog.add(dialogPanel);
+        mainPanel.add(exportPanel);
+        mainPanel.add(exportOptionsPanel);
+
+        mainPanel.add(buttonPanel);
+
+        mainPanel.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        dialog.add(mainPanel);
+        dialog.pack();
         dialog.setVisible(true);
     }
 
     private void filterRadioButtonClicked(ActionEvent e) {
-       //TODO actual statistics logic
+        //TODO: actual statistics logic
         exportStatisticsLabel.setText("Will export 78 out of 90 events.");
         exportStatisticsLabel.setVisible(true);
     }
 
-    private void noFilterRadioButtonClicked(ActionEvent e){
+    private void noFilterRadioButtonClicked(ActionEvent e) {
         exportStatisticsLabel.setText("Will export 90 events.");
         exportStatisticsLabel.setVisible(true);
     }
 
-
-    private void exportButtonClicked(ActionEvent e){
+    private void exportButtonClicked(ActionEvent e) {
         //TODO: actual export logic
         resultFilePath = textField.getText();
         exportFiltered = filterRadioButton.isSelected();
         dialog.dispose();
-
     }
 
-    private void openButtonClicked(ActionEvent e){
+    private void openButtonClicked(ActionEvent e) {
         fileChooser.setFileFilter(filter);
         int result = fileChooser.showSaveDialog(dialog);
-        if(result == JFileChooser.APPROVE_OPTION){
+        if (result == JFileChooser.APPROVE_OPTION) {
             String filePath = fileChooser.getSelectedFile().getAbsolutePath();
             if (!filePath.toLowerCase().endsWith(".json")) {
                 filePath += ".json";
             }
             File file = new File(filePath);
             textField.setText(file.getAbsolutePath());
-
-        }else{
+        } else {
             textField.setText("");
         }
     }
 
-    public String getResultFilePath(){
+    public String getResultFilePath() {
         return resultFilePath;
     }
-    public boolean getExportFiltered(){
+
+    public boolean getExportFiltered() {
         return exportFiltered;
     }
 }
-
