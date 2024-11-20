cfc57ff258f9bb49e872a18db039915ab2d021bd
Šimon Brauner <xbrauner@fi.muni.cz>
2024-11-20 15:45:36 +0100

    Reflect app properties in db schema

--- src/main/resources/cz/muni/fi/pv168/project/storage/sql/db/init.sql
+++ src/main/resources/cz/muni/fi/pv168/project/storage/sql/db/init.sql
@@ -1,20 +1,20 @@
 CREATE TABLE IF NOT EXISTS Category (
     id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(25) NOT NULL,
     color VARCHAR(7) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS TimeUnit (
     id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    shortcut VARCHAR(50) NOT NULL,
+    name VARCHAR(25) NOT NULL,
+    shortcut VARCHAR(4) NOT NULL,
     minutes INT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS Template (
     id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    details TEXT,
+    name VARCHAR(25) NOT NULL,
+    details VARCHAR(36),
     startTime TIME NOT NULL,
     timeUnit UUID NOT NULL,
     timeUnitAmount INT NOT NULL,
@@ -25,24 +25,24 @@ CREATE TABLE IF NOT EXISTS Template_Category (
     template_id UUID NOT NULL,
     category_id UUID NOT NULL,
     PRIMARY KEY (template_id, category_id),
-    FOREIGN KEY (template_id) REFERENCES Template(id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES Category(id) ON DELETE CASCADE
+    FOREIGN KEY (template_id) REFERENCES Template(id),
+    FOREIGN KEY (category_id) REFERENCES Category(id)
 );
 
 CREATE TABLE IF NOT EXISTS TodoEvent (
     id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    details TEXT,
+    name VARCHAR(25) NOT NULL,
+    details VARCHAR(36),
     start TIMESTAMP NOT NULL,
     timeUnit UUID NOT NULL,
     done BOOLEAN DEFAULT FALSE NOT NULL,
-    FOREIGN KEY (timeUnit) REFERENCES TimeUnit(id) ON DELETE CASCADE
+    FOREIGN KEY (timeUnit) REFERENCES TimeUnit(id)
 );
 
 CREATE TABLE IF NOT EXISTS TodoEvent_Category (
     todo_event_id UUID NOT NULL,
     category_id UUID NOT NULL,
     PRIMARY KEY (todo_event_id, category_id),
-    FOREIGN KEY (todo_event_id) REFERENCES TodoEvent(id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES Category(id) ON DELETE CASCADE
+    FOREIGN KEY (todo_event_id) REFERENCES TodoEvent(id),
+    FOREIGN KEY (category_id) REFERENCES Category(id)
 );
