37f87e113f55eb943c4a0920c27458525577d2a3
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2024-11-22 23:40:33 +0100

    refactor: get rid of debug print

--- src/main/java/cz/muni/fi/pv168/project/business/service/export/JSONFileImporter.java
+++ src/main/java/cz/muni/fi/pv168/project/business/service/export/JSONFileImporter.java
@@ -206,12 +206,10 @@ public class JSONFileImporter implements BatchImporter {
             return result;
         }
 
-
         int minute = minutesResult.getData();
         int hour = hoursResult.getData();
 
         LocalTime startTime = LocalTime.of(hour,minute,0, 0);
-
         result.setSuccess(true);
         result.setData(startTime);
         return result;
