e77dfb4ff897aee4e2fde35969c30379f85b11fe
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2024-11-26 10:47:14 +0100

    Bugfix: identical template start times were firing duplicate dialog

--- src/main/java/cz/muni/fi/pv168/project/model/Template.java
+++ src/main/java/cz/muni/fi/pv168/project/model/Template.java
@@ -7,6 +7,7 @@ import cz.muni.fi.pv168.project.business.service.export.serialize.LocalTimeSeria
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
 
@@ -119,8 +120,14 @@ public class Template extends Entity {
     public boolean isMeaningfullyDifferent(Entity e) {
         if (e == null || getClass() != e.getClass()) return true;
         Template template = (Template) e;
+
+        //since inside the system datetime is granular all the way to nanoseconds, need to only take hours and minutes
+        //i.e. a template is meaningfully different when the hour and minute of its start time differs, not nanoseconds
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
+        String timeString = getStartTime().format(timeFormatter);
+
         if (Objects.equals(name, template.name) && Objects.equals(details, template.details) &&
-            Objects.equals(template.getStartTime().toString(), getStartTime().toString()) &&
+            Objects.equals(template.getStartTime().toString(), timeString) &&
             !(getInterval().getTimeUnit().isMeaningfullyDifferent(template.getInterval().getTimeUnit())) &&
             getInterval().getAmount() == template.getInterval().getAmount()) {
                 for (Category category : categories){
