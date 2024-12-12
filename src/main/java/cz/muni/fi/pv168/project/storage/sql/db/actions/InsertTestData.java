1746834715d7c2617dcd53378866a1bd0fa6d4e5
terezahr <91895985+terezahr@users.noreply.github.com>
2024-12-12 13:11:18 +0100

    use dependency provider to insert test data

--- src/main/java/cz/muni/fi/pv168/project/storage/sql/db/actions/InsertTestData.java
+++ src/main/java/cz/muni/fi/pv168/project/storage/sql/db/actions/InsertTestData.java
@@ -1,41 +1,25 @@
 package cz.muni.fi.pv168.project.storage.sql.db.actions;
 
 import cz.muni.fi.pv168.project.data.TestDataGenerator;
-import cz.muni.fi.pv168.project.storage.sql.CategorySqlRepository;
-import cz.muni.fi.pv168.project.storage.sql.TimeUnitSqlRepository;
-import cz.muni.fi.pv168.project.storage.sql.dao.CategoryDao;
-import cz.muni.fi.pv168.project.storage.sql.dao.TimeUnitDao;
-import cz.muni.fi.pv168.project.storage.sql.db.DatabaseManager;
-import cz.muni.fi.pv168.project.storage.sql.db.TransactionConnectionSupplier;
-import cz.muni.fi.pv168.project.storage.sql.db.TransactionExecutorImpl;
-import cz.muni.fi.pv168.project.storage.sql.db.TransactionManagerImpl;
-import cz.muni.fi.pv168.project.storage.sql.entity.mapper.CategoryMapper;
-import cz.muni.fi.pv168.project.storage.sql.entity.mapper.TimeUnitMapper;
+import cz.muni.fi.pv168.project.wiring.DependencyProvider;
+import cz.muni.fi.pv168.project.wiring.ProductionDependencyProvider;
 
+// TODO: Add TodoEvents and Templates
 public class InsertTestData {
     public static void main(String[] args) {
-        // TODO replace this with wiring
+        DependencyProvider provider = new ProductionDependencyProvider();
+
         var testDataGenerator = new TestDataGenerator();
 
-        DatabaseManager databaseManager = DatabaseManager.createProductionInstance();
-        databaseManager.initSchema();
+//        var todoEventRepository = provider.getTodoEventRepository();
+//        var templateRepository = provider.getTemplateRepository();
+        var categoryRepository = provider.getCategoryRepository();
+        var timeUnitRepository = provider.getTimeUnitRepository();
 
-        var transactionManager = new TransactionManagerImpl(databaseManager);
-        var transactionExecutor = new TransactionExecutorImpl(transactionManager::beginTransaction);
-        var transactionConnectionSupplier = new TransactionConnectionSupplier(transactionManager, databaseManager);
-
-        var categoryDao = new CategoryDao(transactionConnectionSupplier);
-        var categoryMapper = new CategoryMapper();
-        var categoryRepository = new CategorySqlRepository(categoryDao, categoryMapper);
-
-        var timeUnitDao = new TimeUnitDao(transactionConnectionSupplier);
-        var timeUnitMapper = new TimeUnitMapper();
-        var timeUnitRepository = new TimeUnitSqlRepository(timeUnitDao, timeUnitMapper);
-
+//        testDataGenerator.createTodoEvents().forEach(todoEventRepository::create);
+//        testDataGenerator.createTemplates().forEach(templateRepository::create);
         testDataGenerator.createCategories().forEach(categoryRepository::create);
         testDataGenerator.createTimeUnits().forEach(timeUnitRepository::create);
-        // testDataGenerator.createTemplates().forEach(templateRepository::create);
-        // testDataGenerator.createTodoEvents().forEach(todoEventRepository::create);
 
         System.out.println("Test data inserted...");
     }
