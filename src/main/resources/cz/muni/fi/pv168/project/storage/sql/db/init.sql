66880a81cd7c50feadb6f0b16ce0392c3c5b482d
Šimon Brauner <xbrauner@fi.muni.cz>
2025-01-03 10:57:37 +0100

    Format sql init script

--- src/main/resources/cz/muni/fi/pv168/project/storage/sql/db/init.sql
+++ src/main/resources/cz/muni/fi/pv168/project/storage/sql/db/init.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS TimeUnit (
     id UUID PRIMARY KEY,
     name VARCHAR(25) NOT NULL,
     shortcut VARCHAR(4) NOT NULL,
-    minutes         INT                   NOT NULL CHECK (minutes >= 0),
+    minutes INT NOT NULL CHECK (minutes >= 0),
     isSystemDefined BOOLEAN DEFAULT FALSE NOT NULL
 );
 
@@ -35,16 +35,16 @@ CREATE TABLE IF NOT EXISTS TodoEvent (
     name VARCHAR(25) NOT NULL,
     details VARCHAR(36),
     start TIMESTAMP NOT NULL,
-    timeUnit       UUID NOT NULL,
+    timeUnit UUID NOT NULL,
     timeUnitAmount INT  NOT NULL CHECK (timeUnitAmount >= 0),
     done BOOLEAN DEFAULT FALSE NOT NULL,
     FOREIGN KEY (timeUnit) REFERENCES TimeUnit(id)
 );
 
 CREATE TABLE IF NOT EXISTS TodoEvent_Category (
-                                                  todoEvent_id UUID NOT NULL,
+    todoEvent_id UUID NOT NULL,
     category_id UUID NOT NULL,
-                                                  PRIMARY KEY (todoEvent_id, category_id),
-                                                  FOREIGN KEY (todoEvent_id) REFERENCES TodoEvent (id),
+    PRIMARY KEY (todoEvent_id, category_id),
+    FOREIGN KEY (todoEvent_id) REFERENCES TodoEvent (id),
     FOREIGN KEY (category_id) REFERENCES Category(id)
 );
