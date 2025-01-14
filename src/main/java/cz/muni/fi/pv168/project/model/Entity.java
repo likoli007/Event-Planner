968fd8ac0376e7deea91da37077fac85feacfb41
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2025-01-14 09:04:33 +0100

    Change Entity's id generation from UUIDv1 to UUIDv7

--- src/main/java/cz/muni/fi/pv168/project/model/Entity.java
+++ src/main/java/cz/muni/fi/pv168/project/model/Entity.java
@@ -1,9 +1,10 @@
 package cz.muni.fi.pv168.project.model;
 
 import java.util.UUID;
+import com.fasterxml.uuid.Generators;
 
 public abstract class Entity {
-    protected UUID id = UUID.randomUUID();
+    protected UUID id = Generators.timeBasedEpochGenerator().generate();
 
     public UUID getId() {
         return id;
